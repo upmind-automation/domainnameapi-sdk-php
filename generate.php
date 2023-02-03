38026b75cc2f3a536595d55bdf4198dc11ce7080
Harry Lewis <harry@upmind.com>
2023-02-03 16:39:27 +0000

    Add generate.php SDK generator script

---
+++ generate.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * This file generates the SDK based on the DomainNameApi WSDL.
+ *
+ * Usage: `php generate.php`
+ */
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use WsdlToPhp\PackageGenerator\ConfigurationReader\GeneratorOptions;
+use WsdlToPhp\PackageGenerator\Generator\Generator;
+
+// Options definition: the configuration file parameter is optional
+$options = GeneratorOptions::instance(/* '/path/file.yml' */);
+/** @var GeneratorOptions $options */
+$options
+    ->setOrigin('http://api.domainnameapi.com/DomainApi.svc?singleWsdl')
+    ->setDestination('./')
+    ->setSrcDirname('SDK')
+    ->setNamespace('Upmind\\DomainNameApiSdk\\SDK')
+    ->setNamespaceDictatesDirectories(false)
+    ->setGatherMethods(GeneratorOptions::VALUE_NONE)
+    ->setServicesFolder('')
+    ->setStandalone(false);
+
+// Generator instantiation
+$generator = new Generator($options);
+// Package generation
+$generator->generatePackage();
